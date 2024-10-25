fec61fbc5c369efcb2209effa38b19264b4b89d5
koala0905 <6889867+koala0905@users.noreply.github.com>
2024-10-25 14:20:46 -0700

    add singin signup form

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import StoreProvider, { useAppSelector } from './redux';
+import AuthProvider from './authProvider';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
@@ -32,9 +33,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout>
-        {children}
-      </DashboardLayout>
+      <AuthProvider>
+        <DashboardLayout>
+          {children}
+        </DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   )
 }
